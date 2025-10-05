65820e8e052cd3017e888dceda69b0c4f611d3bf
Héctor Sanez <hgsanez17@gmail.com>
2025-10-05 13:31:07 -0500

    feat(user): implement service layer and security config

--- user-service/src/main/java/com/ecommerce/user/service/RoleService.java
+++ user-service/src/main/java/com/ecommerce/user/service/RoleService.java
@@ -1,4 +1,37 @@
 package com.ecommerce.user.service;
 
+import com.ecommerce.user.dto.request.RoleCreateRequestDTO;
+import com.ecommerce.user.dto.response.RoleResponseDTO;
+import com.ecommerce.user.entity.Role;
+
+import java.util.List;
+
+
 public interface RoleService {
+
+
+    RoleResponseDTO createRole(RoleCreateRequestDTO requestDTO);
+
+    RoleResponseDTO getRoleById(Long id);
+
+    RoleResponseDTO getRoleByName(String name);
+
+    List<RoleResponseDTO> getAllRoles();
+
+    RoleResponseDTO updateRole(Long id, RoleCreateRequestDTO requestDTO);
+
+    void deleteRole(Long id);
+
+
+    boolean existsByName(String name);
+
+    /**
+     * Metodo interno para obtener la entidad Role.
+     */
+    Role findRoleEntityById(Long id);
+
+    /**
+     * Metodo interno para obtener la entidad Role por nombre.
+     */
+    Role findRoleEntityByName(String name);
 }
