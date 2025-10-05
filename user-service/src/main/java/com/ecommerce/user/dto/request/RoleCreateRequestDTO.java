62b67a253d612122bccd9967438089585a871923
Héctor Sanez <hgsanez17@gmail.com>
2025-10-05 12:52:12 -0500

    feat(user): initialize user module structure

---
+++ user-service/src/main/java/com/ecommerce/user/dto/request/RoleCreateRequestDTO.java
@@ -0,0 +1,4 @@
+package com.ecommerce.user.dto.request;
+
+public class RoleCreateRequestDTO {
+}
---
+++ user-service/src/main/java/com/ecommerce/user/dto/response/UserDetailResponseDTO.java
@@ -0,0 +1,4 @@
+package com.ecommerce.user.dto.response;
+
+public class UserDetailResponseDTO {
+}
